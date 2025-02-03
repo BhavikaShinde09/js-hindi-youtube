5962fe7f800e25967c0f4f81748cf75c7a197e87
BhavikaShinde09 <bhavikas0904@gmail.com>
2025-02-03 16:12:45 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,36 @@
+const name = "bhavika"
+const repoCount = 50
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("bhavikaa-bh-com")
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf("i"));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "    bhavika      "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://bhavika.com%99jjjjjjjkaxna"
+
+console.log(url.replace('%99', "-"))
+
+console.log(url.includes("bhavika"))
+console.log(url.includes("yok"))
+
+console.log(gameName.split("-"));
+
